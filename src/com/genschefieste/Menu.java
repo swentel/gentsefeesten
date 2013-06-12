3fad1bae5164fcc514da469d39e0c085f3d8abd2
swentel <swentel@realize.be>
2013-06-12 23:11:29 +0200

    Add search box - doesn't search yet though

--- src/com/genschefieste/Menu.java
+++ src/com/genschefieste/Menu.java
@@ -1,12 +1,19 @@
 package com.genschefieste;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TableRow;
 
 public class Menu extends Activity {
+
+    String search_free = "";
+    ProgressDialog dialog;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,10 +28,14 @@ public class Menu extends Activity {
         TableRow manageUpdates = (TableRow) findViewById(R.id.go_to_manage_updates);
         manageUpdates.setId(2);
         manageUpdates.setOnClickListener(actionMenu);
+
+        // Add listener on search button.
+        Button search = (Button) findViewById(R.id.search_button);
+        search.setOnClickListener(onSearch);
     }
 
     /**
-     * onClickListener.
+     * Rows onClickListener.
      */
     private final View.OnClickListener actionMenu = new View.OnClickListener() {
         public void onClick(View v) {
@@ -41,4 +52,41 @@ public class Menu extends Activity {
         }
     };
 
+    /**
+     * search OnClickListener.
+     */
+    private final View.OnClickListener onSearch = new View.OnClickListener() {
+        public void onClick(View v) {
+
+            // Get the input from the form.
+            EditText search_free_edit = (EditText) findViewById(R.id.search);
+            search_free = search_free_edit.getText().toString();
+
+            dialog = ProgressDialog.show(Menu.this, getString(R.string.searching), getString(R.string.please_wait), true);
+            new searchTask().execute();
+        }
+    };
+
+    /**
+     * Search task.
+     */
+    class searchTask extends AsyncTask<Void, Void, String> {
+
+        protected String doInBackground(Void... unused) {
+            String sResponse = "";
+
+            return sResponse;
+        }
+
+        protected void onPostExecute(String sResponse) {
+            /*common.searchPostExecute(getApplicationContext(), sResponse, dialog);
+            if (Common.response_success == Common.SUCCESS) {
+                // Parse restaurants.
+                Common.ParseRestaurants(RestaurantForm.this);
+                // Start new intent.
+                Intent intent = new Intent(RestaurantForm.this, EventResults.class);
+                startActivity(intent);
+            }*/
+        }
+    }
 }
