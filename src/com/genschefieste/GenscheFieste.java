1d9f88bd600c63f7bf2a703379d3a100e9940be2
swentel <swentel@realize.be>
2013-06-12 23:59:48 +0200

    Add network permission and helper functions.

--- src/com/genschefieste/GenscheFieste.java
+++ src/com/genschefieste/GenscheFieste.java
@@ -1,6 +1,10 @@
 package com.genschefieste;
 
 import android.app.Application;
+import android.content.Context;
+import android.net.ConnectivityManager;
+
+import java.util.ArrayList;
 
 public class GenscheFieste extends Application {
 
@@ -9,5 +13,27 @@ public class GenscheFieste extends Application {
     public static String locationsUrl = "";
     public static String categoryUrl = "";
 
+    // Public list of events which we can access from everywhere.
+    public static ArrayList<Event> Events = new ArrayList<Event>();
 
+    // Connectivity manager.
+    private ConnectivityManager cm;
+
+    /**
+     * onCreate().
+     */
+    public void onCreate() {
+        super.onCreate();
+        cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+    }
+
+    /**
+     * Helper function to check if network is available.
+     */
+    public boolean netwerkIsAvailable() {
+        if (cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isAvailable() && cm.getActiveNetworkInfo().isConnected()) {
+            return true;
+        }
+        return false;
+    }
 }
