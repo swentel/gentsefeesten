aab4b4bc6e6d0e5fe12f46970f834c6da86bb574
swentel <swentel@realize.be>
2013-06-19 00:23:24 +0200

    Preferences

--- src/com/genschefieste/Main.java
+++ src/com/genschefieste/Main.java
@@ -11,6 +11,7 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main extends BaseActivity {
@@ -25,22 +26,41 @@ public class Main extends BaseActivity {
         // Get the list view.
         ListView list = (ListView) findViewById(R.id.list);
 
-        // Default settings are also set in preferences.xml
+        // Get settings for main query.
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean filterOnLocation = pref.getBoolean("filter_loc", false);
         String prefLocationId = pref.getString("pref_loc", "230");
+        boolean filterOnCategory = pref.getBoolean("filter_cat", false);
         String prefCategoryId = pref.getString("pref_cat", "15");
         boolean prefShowAllDay = pref.getBoolean("pref_all_day", false);
 
-        // Get main events.
-        DatabaseHandler db = new DatabaseHandler(this);
-        String selectQuery = "SELECT * FROM " + DatabaseHandler.TABLE_EVENTS;
+        // Create query.
+        List<String> whereClauses = new ArrayList<String>();
         // Location and category.
-        // TODO this has to be multiple.
-        selectQuery += " WHERE " + DatabaseHandler.KEY_LOC_ID + " = " + prefLocationId;
-        selectQuery += " AND " + DatabaseHandler.KEY_CAT_ID + " = " + prefCategoryId;
+        if (filterOnLocation || filterOnCategory) {
+            if (filterOnLocation) {
+                whereClauses.add(DatabaseHandler.KEY_LOC_ID + " = " + prefLocationId);
+            }
+            if (filterOnCategory) {
+                whereClauses.add(DatabaseHandler.KEY_CAT_ID + " = " + prefCategoryId);
+            }
+        }
         // Start hour.
         if (!prefShowAllDay) {
-            selectQuery += " AND " + DatabaseHandler.KEY_START_HOUR + " != ''";
+            whereClauses.add(DatabaseHandler.KEY_START_HOUR + " != ''");
+        }
+
+        DatabaseHandler db = new DatabaseHandler(this);
+        String selectQuery = "SELECT * FROM " + DatabaseHandler.TABLE_EVENTS;
+        if (whereClauses.size() > 0) {
+            StringBuilder builder = new StringBuilder();
+            builder.append(whereClauses.remove(0));
+            for( String s : whereClauses) {
+                builder.append( " AND ");
+                builder.append( s);
+            }
+            String statement = builder.toString();
+            selectQuery += " WHERE " + statement;
         }
         selectQuery += " ORDER BY "+ DatabaseHandler.KEY_DATE +" ASC, "+ DatabaseHandler.KEY_DATE_SORT +" ASC limit 30";
         events = db.getEvents(selectQuery);
@@ -77,8 +97,8 @@ public class Main extends BaseActivity {
      */
     private final View.OnClickListener goToPreferences = new View.OnClickListener() {
         public void onClick(View v) {
-            Intent preferences = new Intent(getBaseContext(), Prefs.class);
-            startActivity(preferences);
+        Intent preferences = new Intent(getBaseContext(), Prefs.class);
+        startActivity(preferences);
         }
     };
 
